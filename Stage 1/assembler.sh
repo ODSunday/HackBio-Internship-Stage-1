3c19fa46985f4c71a930d6a6215d0d200937b7c1
Oni David Sunday <sundayonidavid@gmail.com>
2025-09-27 10:29:17 +0100

    Delete Stage 1/assembler.sh

--- Stage 1/assembler.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for sample in $(ls trimmed_samples/*_1_trimmed.fastq.gz | sed 's|.*/||; s/_1_trimmed.fastq.gz//'); do
-    spades.py -1 "trimmed_samples/${sample}_1_trimmed.fastq.gz" \
-              -2 "trimmed_samples/${sample}_2_trimmed.fastq.gz" \
-              -o "assembly/${sample}_spades" \
-	      --threads 4 \
-              --phred-offset 33
-done
